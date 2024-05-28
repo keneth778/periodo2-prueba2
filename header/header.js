9562d3cb8a6491a808138359c83f0439bfb990da
keneth <keneth.javier90@gmail.com>
2024-05-28 09:29:19 -0600

    hecho

---
+++ header/header.js
@@ -0,0 +1,12 @@
+let header = document.querySelector(".header");
+header.innerHTML = `
+<div class="div1">
+<span>bloque izquierdo</span>
+</div>
+<div class="div2">
+<span>header</span>
+</div>
+<div class="div3">
+<span>bloque derecho</span>
+</div>
+`
---
+++ section/section.js
@@ -0,0 +1,19 @@
+let section = document.querySelector(".section");
+section.innerHTML = `
+<div class="sec1">
+<span>aaaaa</span>
+</div>
+<div class="sec2">
+<span>aaaaa</span>
+</div>
+<div class="sec3">
+<span>aaaaa</span>
+</div>
+<div class="sec4">
+<span>aaaaa</span>
+</div>
+<div class="sec5">
+<span>aaaaa</span>
+</div>
+
+`
